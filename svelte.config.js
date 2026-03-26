959a97e12c03c9252f632426a559f0ff46e673d2
DatenMints <febrimicrone@gmail.com>
2026-03-26 17:35:11 +0800

    svelte config modifications

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 // import adapter from '@sveltejs/adapter-auto';
-// import { relative, sep } from 'node:path';
+import { relative, sep } from 'node:path';
 import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
